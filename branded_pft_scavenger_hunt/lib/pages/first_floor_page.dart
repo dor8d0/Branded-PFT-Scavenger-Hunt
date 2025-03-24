d1f311d423d3821ce466a951c89f887fd72e8ad5
dor8d0 <troyw792@gmail.com>
2025-03-24 15:01:01 -0500

    Adds statefulness to the pages

--- branded_pft_scavenger_hunt/lib/pages/first_floor_page.dart
+++ branded_pft_scavenger_hunt/lib/pages/first_floor_page.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
-class FirstFloorPage extends StatelessWidget {
+class FirstFloorPage extends StatefulWidget {
   const FirstFloorPage({super.key});
+
+  @override
+  State<FirstFloorPage> createState() => _FirstFloorPageState();
+}
+
+class _FirstFloorPageState extends State<FirstFloorPage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = [
+    FirstFloorQ1(),
+  ];
+}
+
+//Getting through a page will update the index which will update the state of the main page
+class FirstFloorQ1 extends StatelessWidget {
+  const FirstFloorQ1({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,4 +30,4 @@ class FirstFloorPage extends StatelessWidget {
       ),
     );
   }
-} 
+}

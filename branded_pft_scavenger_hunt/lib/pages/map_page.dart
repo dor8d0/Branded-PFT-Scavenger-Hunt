879a133361887c08e0f324571a9807a6330bc1e4
dor8d0 <troyw792@gmail.com>
2025-03-30 00:52:26 -0500

    Adds map images to map page with a switch button

--- branded_pft_scavenger_hunt/lib/pages/map_page.dart
+++ branded_pft_scavenger_hunt/lib/pages/map_page.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  bool _isFirstFloor = true;
+
+  void _toggleFloor() {
+    setState(() {
+      _isFirstFloor = !_isFirstFloor;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +22,26 @@ class MapPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Map'),
       ),
-      body: const Center(
-        child: Text('Map Content'),
+      body: Column(
+        children: [
+          Expanded(
+            child: InteractiveViewer(
+              minScale: 0.5,
+              maxScale: 4.0,
+              child: Image.asset(
+                _isFirstFloor ? 'images/pftmap1.webp' : 'images/pftmap2.webp',
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: _toggleFloor,
+              child: const Text('Switch Floors'),
+            ),
+          ),
+        ],
       ),
     );
   }
